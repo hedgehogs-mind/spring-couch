7b593fda30bca9e2838e8a8632b900b7abdfd509
hedgehogs-mind <peter@hedgehogs-mind.com>
2020-11-14 14:28:36 +0100

    Initial Spring project

---
+++ src/main/java/com/hedgehogsmind/springcouch2r/SpringCouch2rApplication.java
@@ -0,0 +1,13 @@
+package com.hedgehogsmind.springcouch2r;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCouch2rApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCouch2rApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hedgehogsmind/springcouch2r/SpringCouch2rApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hedgehogsmind.springcouch2r;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCouch2rApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
