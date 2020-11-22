7b5e8032431bf4b05a9d9ac57450209235b335b7
hedgehogs-mind <peter@hedgehogs-mind.com>
2020-11-22 14:45:50 +0100

    Added tests for response writing

--- src/main/java/com/hedgehogsmind/springcouch2r/util/Couch2rResponseUtil.java
+++ src/main/java/com/hedgehogsmind/springcouch2r/util/Couch2rResponseUtil.java
@@ -2,7 +2,6 @@ package com.hedgehogsmind.springcouch2r.util;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.hedgehogsmind.springcouch2r.rest.problemdetail.ProblemDescriptor;
 import com.hedgehogsmind.springcouch2r.rest.problemdetail.ProblemDetail;
 import com.hedgehogsmind.springcouch2r.rest.problemdetail.ProblemDetailConvertible;
 import org.springframework.http.MediaType;
@@ -17,7 +16,50 @@ import java.util.stream.Collectors;
 
 public final class Couch2rResponseUtil {
 
-    // TODO @peter docs
+    /**
+     * <p>
+     *     To sum it up: Writes a ResponseEntity to the response.
+     * </p>
+     *
+     * <p>
+     *     First fetches tries to fetch locale from request. If none is specified, {@link Locale#ENGLISH} is used.
+     * </p>
+     *
+     * <p>
+     *     Then status and headers will be written to the response object.
+     * </p>
+     *
+     * <p>
+     *     Last step is to write the body to the response (in case it is not null).
+     *     This step is a bit more complex: First the body will be converted into a String:
+     *
+     *     <ul>
+     *         <li>
+     *             Body is {@link String}, {@link Boolean} or {@link Number},
+     *             then the value will be written as a plain String.
+     *         </li>
+     *         <li>
+     *             Body is assignable to {@link ProblemDetailConvertible}:
+     *             New ProblemDetail instance is created using fetched locale.
+     *             Then this instance is used as if the body is a already a ProblemDetail.
+     *             See next case.
+     *         </li>
+     *         <li>
+     *             Body is a ProblemDetail: Use {@link ProblemDetail#serializeAsJson()} as body.
+     *         </li>
+     *         <li>
+     *             Otherwise: We use the given {@link ObjectMapper} to serialize the body as a String.
+     *         </li>
+     *     </ul>
+     *
+     *     Then the character encoding is set to UTF-8 and the body written using the response's writer.
+     * </p>
+     *
+     * @param responseEntity ResponseEntity to write to response.
+     * @param request Request to check for locale.
+     * @param response Response to write ResponseEntity to.
+     * @param objectMapper ObjectMapper used to serialize generic object body.
+     */
     public static void writeResponseEntity(
             final ResponseEntity responseEntity,
             final HttpServletRequest request,
@@ -56,7 +98,7 @@ public final class Couch2rResponseUtil {
                     problemDetail = (ProblemDetail) body;
                 }
 
-                bodyToWrite = ((ProblemDetail) body).serializeAsJson();
+                bodyToWrite = problemDetail.serializeAsJson();
                 response.setContentType(ProblemDetail.MEDIA_TYPE.toString());
 
             } else {
@@ -71,6 +113,7 @@ public final class Couch2rResponseUtil {
             if ( bodyToWrite == null ) throw new IllegalStateException("Body stringify failed.");
 
             response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+
             try {
                 response.getWriter().print(bodyToWrite);
             } catch ( IOException e ) {
